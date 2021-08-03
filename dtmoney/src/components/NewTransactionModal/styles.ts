a19c843e50d7c9e50963e2fa05614f6461610561
Miguel Machado <miiguellmp@gmail.com>
2021-08-03 10:00:02 -0300

    feat(button): add color button

--- dtmoney/src/components/NewTransactionModal/styles.ts
+++ dtmoney/src/components/NewTransactionModal/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import { darken } from "polished";
+import { darken, transparentize } from "polished";
 
 export const Container = styled.form``;
 
@@ -55,33 +55,44 @@ export const TransactionTypeContainer = styled.div`
 `;
 
 type RadioxBox = {
-  isActive: boolean
-}
+  isActive: boolean;
+  activeColor: "green" | "red";
+};
+
+const colors = {
+  green: "#33CC95",
+  red: "#E52E4D",
+};
 
 export const RadioBox = styled.button<RadioxBox>`
   height: 4rem;
-    border: 1px solid #d7d7d7;
-    border-radius: 0.25rem;
-    background: ${(props) => props.isActive ? '#eee' : 'transparent'};
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  border: 1px solid #d7d7d7;
+  border-radius: 0.25rem;
 
-    transition: border-color 0.2s;
+  background: ${(props) =>
+    props.isActive
+      ? transparentize(0.9, colors[props.activeColor])
+      : "transparent"};
 
-    &:hover {
-      border-color: ${darken(0.1, "#d7d7d7")};
-    }
+  display: flex;
+  align-items: center;
+  justify-content: center;
 
-    img {
-      width: 20px;
-      height: 20px;
-    }
+  transition: border-color 0.2s;
 
-    span {
-      display: inline-block;
-      margin-left: 1rem;
-      font-size: 1rem;
-      color: var(--text-title);
-    }
+  &:hover {
+    border-color: ${darken(0.1, "#d7d7d7")};
+  }
+
+  img {
+    width: 20px;
+    height: 20px;
+  }
+
+  span {
+    display: inline-block;
+    margin-left: 1rem;
+    font-size: 1rem;
+    color: var(--text-title);
+  }
 `;
