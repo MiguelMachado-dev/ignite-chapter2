c35c6b053cb2510c00a17e57738730d8bccb6d0b
Miguel Machado <miiguellmp@gmail.com>
2021-08-03 11:10:19 -0300

    feat(format): format values - amount and date

--- dtmoney/src/components/TransactionsTable/index.tsx
+++ dtmoney/src/components/TransactionsTable/index.tsx
@@ -17,7 +17,9 @@ export function TransactionsTable() {
   const [transactions, setTransactions] = useState<Array<ITransaction>>([]);
 
   useEffect(() => {
-    api.get("/transactions").then((response) => setTransactions(response.data.transactions));
+    api
+      .get("/transactions")
+      .then((response) => setTransactions(response.data.transactions));
   }, []);
 
   return (
@@ -36,9 +38,18 @@ export function TransactionsTable() {
           {transactions.map((transaction) => (
             <tr key={transaction.id}>
               <td>{transaction.title}</td>
-              <td className={transaction.type}>{transaction.amount}</td>
+              <td className={transaction.type}>
+                {new Intl.NumberFormat("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                }).format(transaction.amount)}
+              </td>
               <td>{transaction.category}</td>
-              <td>{transaction.createdAt}</td>
+              <td>
+                {new Intl.DateTimeFormat("pt-BR").format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
             </tr>
           ))}
         </tbody>
