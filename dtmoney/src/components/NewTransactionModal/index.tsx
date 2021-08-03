406e17617d3311b0a7093eb6f030b44bdf55d315
Miguel Machado <miiguellmp@gmail.com>
2021-08-02 22:07:53 -0300

    feat(modal): create modal structure

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,5 +1,6 @@
 import Modal from "react-modal";
 
+import * as S from "./styles";
 interface INewTransactonModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
@@ -12,8 +13,21 @@ export function NewTransactionModal({
   onRequestClose,
 }: INewTransactonModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
-      <h2>Cadastrar Transação</h2>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <S.Container>
+        <h2>Cadastrar Transação</h2>
+
+        <input placeholder="Título" type="text" />
+        <input placeholder="Valor" type="number" />
+        <input placeholder="Categoria" type="text" />
+
+        <button type="submit">Cadastrar</button>
+      </S.Container>
     </Modal>
   );
 }
