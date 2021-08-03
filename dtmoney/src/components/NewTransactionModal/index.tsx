89140d8a01be0e1c8cd64baf350151975669dda5
Miguel Machado <miiguellmp@gmail.com>
2021-08-03 10:40:05 -0300

    feat(form): save form data

--- dtmoney/src/components/NewTransactionModal/index.tsx
+++ dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import Modal from "react-modal";
 
 import closeImg from "../../assets/close.svg";
@@ -16,7 +16,18 @@ export function NewTransactionModal({
   isOpen,
   onRequestClose,
 }: INewTransactonModalProps) {
+  const [title, setTitle] = useState("");
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState("");
   const [type, setType] = useState<string>("deposit");
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      title, value, category, type
+    })
+  }
 
   return (
     <Modal
@@ -33,11 +44,22 @@ export function NewTransactionModal({
         <img src={closeImg} alt="Fechar modal" />
       </button>
 
-      <S.Container>
+      <S.Container onSubmit={handleCreateNewTransaction}>
         <S.FormTitle>Cadastrar Transação</S.FormTitle>
 
-        <S.Input placeholder="Título" type="text" />
-        <S.Input placeholder="Valor" type="number" />
+        <S.Input
+          placeholder="Título"
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+
+        <S.Input
+          placeholder="Valor"
+          type="number"
+          value={value}
+          onChange={(event) => setValue(Number(event.target.value))}
+        />
 
         <S.TransactionTypeContainer>
           <S.RadioBox
@@ -65,7 +87,12 @@ export function NewTransactionModal({
           </S.RadioBox>
         </S.TransactionTypeContainer>
 
-        <S.Input placeholder="Categoria" type="text" />
+        <S.Input
+          placeholder="Categoria"
+          type="text"
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        />
 
         <S.Submit type="submit">Cadastrar</S.Submit>
       </S.Container>
